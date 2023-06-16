84dced3aedda08be649b61923811420d5e4625ee
Gabriel Boniolo <gabrielboniolo@gmail.com>
2023-06-15 21:32:35 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Sorting_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o tamanho do vetor: ");
+            int tam = int.Parse(Console.ReadLine());
+
+            int[] vetor = new int[tam];
+
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write($"Digite um número aleatório para a posição {i}: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Sort.Selection(vetor);
+
+            foreach(int n in vetor)
+            {
+                Console.Write(n + " ");
+            }
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_System
+{
+    internal class Sort
+    {
+        public static int[] Selection(int[] vetor)
+        {
+            int aux;
+
+            for(int i = 0; i < vetor.Length - 1; i++)
+            {
+                for(int j = i + 1; j < vetor.Length; j++)
+                {
+                    if (vetor[i] > vetor[j])
+                    {
+                        aux = vetor[i];
+                        vetor[i] = vetor[j];
+                        vetor[j] = aux;
+                    }
+                }
+            }
+            return vetor;
+        }
+
+        public static int[] Bubble(int[] vetor)
+        {
+            int aux;
+            for(int i = 0; i < vetor.Length - 1; i ++)
+            {
+                for(int j = 0; j < vetor.Length - i - 1; j++)
+                {
+                    if(vetor[j] > vetor[j + 1]) {
+                        aux = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = aux;
+                    }
+                }
+            }
+            return vetor;
+        }
+
+    }
+}
